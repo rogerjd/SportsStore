0c283f28c758eefecb3aad859147a79567e2c0b1
roger <rjpd@netzero.com>
2016-06-18 19:48:03 -0400

    categories

---
+++ SportsStore.UnitTests/Controllers/Categories.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Interface;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests.Controllers
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            //init
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID=1, Name="P1", Category="Apples" },
+                new Product {ProductID=2, Name="P2", Category="Apples" },
+                new Product {ProductID=3, Name="P3", Category="Plums" },
+                new Product {ProductID=4, Name="P4", Category="Oranges"},
+            }.AsQueryable());
+
+            NavController target = new NavController(mock.Object);
+
+            //run
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            //done
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,30 @@
+﻿using SportsStore.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        IProductRepository repository;
+
+        public NavController(IProductRepository repos)
+        {
+            repository = repos;
+        }
+
+        // GET: Nav
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
