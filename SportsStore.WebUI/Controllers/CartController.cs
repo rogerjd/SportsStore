b78ccb2b53307856740ab6075f5458c0ba79a6f3
roger <rjpd@netzero.com>
2016-06-20 00:02:29 -0400

    cart work

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using SportsStore.Domain.Entities;
+using SportsStore.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        IProductRepository repository;
+
+        public CartController(IProductRepository repos)
+        {
+            repository = repos;
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId); //how can be default?
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId); //how can be default?
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+    }
+}
